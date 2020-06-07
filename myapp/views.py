9704750245332b1cdb4c443536e29ca534d679d0
rishabgoyal368 <rishabgoyal368@gmail.com>
2020-06-07 11:46:28 +0530

    first comit

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+# Create your views here.
+
+
+def scrap(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    link_address = []
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+    return render(request, 'myapp/result.html', {'link_address': link_address})
